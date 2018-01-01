f78570ea9526c81c0504fac31f308d755cc2321d
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2018-01-02 01:33:00 +0900

    use plus plugin

--- example/a+b/cpp-TLE/main.cc
+++ example/a+b/cpp-TLE/main.cc
@@ -5,13 +5,15 @@ using namespace std;
 int main() {
   int a, b;
   cin >> a >> b;
-  int c = 0;
-  for (int i = 0; i < a; ++i) {
-    c++;
+  long long c = 0;
+  for (int k = 0; k < 10; ++k) {
+    for (int i = 0; i < a; ++i) {
+      c++;
+    }
+    for (int i = 0; i < b; ++i) {
+      c++;
+    }
   }
-  for (int i = 0; i < b; ++i) {
-    c++;
-  }
-  cout << c << endl;
+  cout << c / 10 << endl;
   return 0;
 }
