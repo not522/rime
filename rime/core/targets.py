dcf7d26fb413a5583c525c17e00cd06994c2f471
Naoto Mizuno <mizuno@eps.s.u-tokyo.ac.jp>
2020-05-04 07:08:17 +0900

    Use basic's project

--- rime/core/targets.py
+++ rime/core/targets.py
@@ -2,6 +2,7 @@ import os
 import os.path
 import sys
 
+from rime.core import taskgraph
 from rime.util import class_registry
 from rime.util import files
 from rime.util import module_loader
@@ -98,6 +99,9 @@ class ReloadConfiguration(Exception):
     pass
 
 
+libdir = None
+
+
 class Project(TargetBase):
     """Project target.
 
@@ -106,6 +110,11 @@ class Project(TargetBase):
     """
 
     CONFIG_FILENAME = 'PROJECT'
+
+    def __init__(self, name, base_dir, parent):
+        assert parent is None
+        super(Project, self).__init__(name, base_dir, parent)
+        self.project = self
 
     def PreLoad(self, ui):
         # Do not use super() here because targets.Project will be overridden.
@@ -119,6 +128,68 @@ class Project(TargetBase):
                         'Failed to load a plugin: %s' % name)
                 raise ReloadConfiguration('use_plugin(%s)' % repr(name))
         self.exports['use_plugin'] = use_plugin
+
+        self.library_dir = None
+        self.project_defined = False
+
+        def _project(library_dir=None):
+            if self.project_defined:
+                raise RuntimeError('project() is already defined.')
+            global libdir
+            libdir = os.path.join(
+                self.base_dir,
+                library_dir)
+            self.library_dir = libdir
+            self.project_defined = True
+        self.exports['project'] = _project
+
+    def PostLoad(self, ui):
+        super(Project, self).PostLoad(ui)
+        self._ChainLoad(ui)
+
+    def _ChainLoad(self, ui):
+        # Chain-load problems.
+        self.problems = []
+        for name in files.ListDir(self.base_dir):
+            path = os.path.join(self.base_dir, name)
+            if targets.registry.Problem.CanLoadFrom(path):
+                problem = targets.registry.Problem(name, path, self)
+                try:
+                    problem.Load(ui)
+                    self.problems.append(problem)
+                except targets.ConfigurationError:
+                    ui.errors.Exception(problem)
+        self.problems.sort(key=lambda a: (a.id, a.name))
+
+    def FindByBaseDir(self, base_dir):
+        if self.base_dir == base_dir:
+            return self
+        for problem in self.problems:
+            obj = problem.FindByBaseDir(base_dir)
+            if obj:
+                return obj
+        return None
+
+    @taskgraph.task_method
+    def Build(self, ui):
+        """Build all problems."""
+        results = yield taskgraph.TaskBranch(
+            [problem.Build(ui) for problem in self.problems])
+        yield all(results)
+
+    @taskgraph.task_method
+    def Test(self, ui):
+        """Run tests in the project."""
+        results = yield taskgraph.TaskBranch(
+            [problem.Test(ui) for problem in self.problems])
+        yield list(itertools.chain(*results))
+
+    @taskgraph.task_method
+    def Clean(self, ui):
+        """Clean the project."""
+        results = yield taskgraph.TaskBranch(
+            [problem.Clean(ui) for problem in self.problems])
+        yield all(results)
 
 
 registry = class_registry.ClassRegistry(TargetBase)
